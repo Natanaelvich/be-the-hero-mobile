3b4c3f352baf4db21e06c289887d93bc5a9aae8b
natanaelvich <taelima1997@gmail.com>
2020-03-28 00:45:21 -0300

    create detali page and yours styles

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,7 +1,66 @@
 import React from 'react';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import {
+  Container,
+  Header,
+  ButtonBack,
+  Incident,
+  Property,
+  Value,
+  Logo,
+  ContactBox,
+  HeroeTitle,
+  HeroeDescription,
+  Actions,
+  ButtonAction,
+  ActionText,
+} from './styles';
+
+import logo from '~/assets/logo.png';
 
 export default function Detail() {
-  return <Container />;
+  const navigation = useNavigation();
+
+  function navigateToIncident() {
+    navigation.goBack();
+  }
+  return (
+    <Container>
+      <Header>
+        <Logo source={logo} />
+
+        <ButtonBack onPress={navigateToIncident}>
+          <Feather name="arrow-left" size={28} color="#e02041" />
+        </ButtonBack>
+      </Header>
+
+      <Incident>
+        <Property>ONG:</Property>
+        <Value>APAD</Value>
+
+        <Property>CASO</Property>
+        <Value>Cadelinha com corona virus</Value>
+
+        <Property>VALOR:</Property>
+        <Value>R$ 200</Value>
+      </Incident>
+
+      <ContactBox>
+        <HeroeTitle>Salve o dia!</HeroeTitle>
+        <HeroeDescription>Seja o heroi desse caso.</HeroeDescription>
+
+        <Actions>
+          <ButtonAction>
+            <ActionText>Whatsapp</ActionText>
+          </ButtonAction>
+
+          <ButtonAction>
+            <ActionText>E-mail</ActionText>
+          </ButtonAction>
+        </Actions>
+      </ContactBox>
+    </Container>
+  );
 }
--- src/pages/Detail/styles.js
+++ src/pages/Detail/styles.js
@@ -1,3 +1,69 @@
 import styled from 'styled-components/native';
+import Constants from 'expo-constants';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  padding: 0 20px;
+  padding-top: ${Constants.statusBarHeight + 20}px;
+`;
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const ButtonBack = styled.TouchableOpacity``;
+export const Logo = styled.Image``;
+export const Incident = styled.View`
+  padding: 24px;
+  border-radius: 8px;
+  background: #fff;
+  margin-bottom: 16px;
+  margin-top: 24px;
+`;
+export const Property = styled.Text`
+  margin-top: 8px;
+  font-size: 14px;
+  color: #333;
+  font-weight: bold;
+`;
+export const Value = styled.Text`
+  margin-top: 8px;
+  font-size: 15px;
+  margin-bottom: 24px;
+  color: #666;
+`;
+export const ContactBox = styled.View`
+  padding: 24px;
+  border-radius: 8px;
+  background: #fff;
+  margin-bottom: 16px;
+`;
+export const HeroeTitle = styled.Text`
+  font-weight: bold;
+  font-size: 20px;
+  color: #111;
+  line-height: 30px;
+`;
+export const HeroeDescription = styled.Text`
+  font-size: 15px;
+  color: #666;
+  margin-top: 16px;
+`;
+export const Actions = styled.View`
+  margin-top: 16px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+export const ButtonAction = styled.TouchableOpacity`
+  background: #e02041;
+  border-radius: 8px;
+  height: 50px;
+  width: 48%;
+  justify-content: center;
+  align-items: center;
+`;
+export const ActionText = styled.Text`
+  color: #fff;
+  font-size: 15px;
+  font-weight: bold;
+`;
